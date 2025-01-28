c86033ec1eae0249d52318ca9c28e798c37987e6
cailaar <caila.alyssa.ryner@gmail.com>
2025-01-28 21:55:13 +0000

    tidy

--- src/UserList.test.tsx
+++ src/UserList.test.tsx
@@ -1,57 +1,51 @@
-import { render, screen, within } from '@testing-library/react';
-import UserList from './UserList';
-
+import { render, screen, within } from "@testing-library/react";
+import UserList from "./UserList";
 
 // Defining a render component function for setup, not advised to render in a beforeEach
 const renderComponent = () => {
-    const users = [ 
-        {name: 'jane', email: 'jane@email.com'},
-        {name: 'sam', email: 'sam@email.cpm'}
-    ]
-    const {container} = render(<UserList users={users} />);
+  const users = [
+    { name: "jane", email: "jane@email.com" },
+    { name: "sam", email: "sam@email.com" },
+  ];
+  const { container } = render(<UserList users={users} />);
 
-    return {container, users}
-}
+  return { container, users };
+};
 
+test("render one row per user (data-testid method)", () => {
+  // Render the component
+  renderComponent();
 
-test('render one row per user (data-testid method)', () => {
-    // Render the component
-    renderComponent();
+  //Find all the rows in the table
+  const rows = within(screen.getByTestId("users")).getAllByRole("row");
 
-    //Find all the rows in the table
-    const rows = within(screen.getByTestId('users')).getAllByRole('row');
+  // screen.logTestingPlaygroundURL(); can give recommendations for queries
 
-    // screen.logTestingPlaygroundURL(); can give recommendations for queries
-
-    //Assertion: correct number of rows in the table
-    expect(rows).toHaveLength(2);
+  //Assertion: correct number of rows in the table
+  expect(rows).toHaveLength(2);
 });
 
-test('render one row per user (container method)', () => {
-    // Render the component
-    const {container} =renderComponent();
+test("render one row per user (container method)", () => {
+  // Render the component
+  const { container } = renderComponent();
 
+  //Find all the rows in the table
+  // we can use css selectors to select the specific elements
+  //eslint-disable-next-line
+  const rows = container.querySelectorAll("tbody tr");
 
-    //Find all the rows in the table
-    // we can use css selectors to select the specific elements
-    //eslint-disable-next-line
-    const rows = container.querySelectorAll('tbody tr');
-
-    //Assertion: correct number of rows in the table
-    expect(rows).toHaveLength(2);
+  //Assertion: correct number of rows in the table
+  expect(rows).toHaveLength(2);
 });
 
+test("render the email and name of each user", () => {
+  const { users } = renderComponent();
 
-test('render the email and name of each user', () => {
-    const {users} =renderComponent();
+  for (let user of users) {
+    const name = screen.getByRole("cell", { name: user.name });
+    const email = screen.getByRole("cell", { name: user.email });
 
-
-    for (let user of users) {
-        const name = screen.getByRole('cell', {name: user.name});
-        const email = screen.getByRole('cell', {name: user.email});
-        
-        expect(name).toBeInTheDocument();
-        expect(email).toBeInTheDocument();
-        
-    }
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
 });
